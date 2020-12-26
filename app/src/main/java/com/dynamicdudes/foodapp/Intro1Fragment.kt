595874bcb2e5e8d5eec9f57cd2d2aec26868d6a2
Tharun110 <72254564+Tharun110@users.noreply.github.com>
2020-12-26 08:14:40 +0530

    Initial Commit

---
+++ app/src/main/java/com/dynamicdudes/foodapp/Intro1Fragment.kt
@@ -0,0 +1,19 @@
+package com.dynamicdudes.foodapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Intro1Fragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_intro1, container, false)
+    }
+
+}
